d54ed477eb10d1c172f0810fb8b8bd6b53a3f7d3
XLG123 <xiaolin.guan123@gmail.com>
2023-05-19 13:47:42 -0400

    Implemented PolyTreeNode Class Phase 1 & 2

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,17 @@
 class PolyTreeNode
-  
+  attr_reader :children, :value, :parent
+
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+
+  def parent=(person)
+    @parent.children.delete(self) if !@parent.nil?
+    @parent = person
+    if !@parent.nil? && !@parent.children.include?(self)
+      @parent.children << self
+    end
+  end
 end
